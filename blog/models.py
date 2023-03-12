bc0b04c0a2e0e5b54ab91bbf1c453a9e567560e8
AlinaPanasenko <maikshev@gmail.com>
2023-03-12 22:25:25 +0000

    Add view for Post

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+STATUS = ((0, "Draft"), (1, "Published"))
+
+
+def deleted_user():
+    return User.objects.get(username="deleted")
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete=models.SET(deleted_user), related_name="blog_posts")
+    recipe = models.TextField()
+    created_on = models.DateField(auto_now_add=True)
+    updated_on = models.DateField(auto_now_add=True)
+    featured_image = CloudinaryField('image', default='placeholder')
+    excerpt = models.TextField(blank=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    likes = models.ManyToManyField(User, related_name='blog_likes', blank=True)
+
+
+    class Meta:
+        ordering = ['-created_on']
+
+
+    def __str__(self):
+        return self.title
+
+    def number_of_likes(self):
+        return self.likes.count()
+
+
+    
