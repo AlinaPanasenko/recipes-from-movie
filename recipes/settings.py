c085aa600bfef726be86fff8de93b83940468e36
AlinaPanasenko <maikshev@gmail.com>
2023-04-03 22:07:27 +0000

    Deployment

--- recipes/settings.py
+++ recipes/settings.py
@@ -28,7 +28,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
+X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = ['recipes-from-movies.herokuapp.com', 'localhost']
 
